0770bc07be95495224cb58817e4d7ebba4a786e5
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-29 00:20:46 +0900

    DirectXの初期化と解放

--- main.cpp
+++ main.cpp
@@ -1,4 +1,92 @@
 #include <Windows.h>
+#include <vector>
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
+
+// 安全にRELEASE_SAFEease関数を実行するためのマクロ
+#define RELEASE_SAFE(p) if (p != nullptr) { p->Release(); p = nullptr; }
+
+ID3D12Device* pDevice = nullptr;
+IDXGIFactory6* pDxgiFactory = nullptr;
+ID3D12CommandAllocator* pCommandAllocator = nullptr;
+ID3D12GraphicsCommandList* pCommandList = nullptr;
+ID3D12CommandQueue* pCommandQueue = nullptr;
+IDXGISwapChain4* pSwapChain = nullptr;
+ID3D12DescriptorHeap* pDescriptorHeap = nullptr;
+
+const int WINDOW_WIDTH = 640;
+const int WINDOW_HEIGHT = 480;
+
+// DirectXの初期化
+void InitD3DX(HWND hWnd)
+{
+	D3D12CreateDevice(nullptr, D3D_FEATURE_LEVEL_12_1, IID_PPV_ARGS(&pDevice));
+	CreateDXGIFactory1(IID_PPV_ARGS(&pDxgiFactory));
+	pDevice->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&pCommandAllocator));
+	pDevice->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, pCommandAllocator, nullptr, IID_PPV_ARGS(&pCommandList));
+
+	{
+		D3D12_COMMAND_QUEUE_DESC desc = {};
+		desc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+		desc.NodeMask = 0;
+		desc.Priority = D3D12_COMMAND_QUEUE_PRIORITY_NORMAL;
+		desc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+		pDevice->CreateCommandQueue(&desc, IID_PPV_ARGS(&pCommandQueue));
+	}
+
+	{
+		DXGI_SWAP_CHAIN_DESC1 desc = {};
+		desc.Width = WINDOW_WIDTH;
+		desc.Height = WINDOW_HEIGHT;
+		desc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+		desc.Stereo = false;
+		desc.SampleDesc.Count = 1;
+		desc.SampleDesc.Quality = 0;
+		desc.BufferUsage = DXGI_USAGE_BACK_BUFFER;
+		desc.BufferCount = 2;
+		desc.Scaling = DXGI_SCALING_STRETCH;
+		desc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+		desc.AlphaMode = DXGI_ALPHA_MODE_UNSPECIFIED;
+		desc.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+		pDxgiFactory->CreateSwapChainForHwnd(pCommandQueue, hWnd, &desc, nullptr, nullptr, (IDXGISwapChain1**)&pSwapChain);
+	}
+
+	{
+		D3D12_DESCRIPTOR_HEAP_DESC desc = {};
+		desc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+		desc.NodeMask = 0;
+		desc.NumDescriptors = 2;
+		desc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+		pDevice->CreateDescriptorHeap(&desc, IID_PPV_ARGS(&pDescriptorHeap));
+	}
+
+	{
+		DXGI_SWAP_CHAIN_DESC desc = {};
+		pSwapChain->GetDesc(&desc);
+		std::vector<ID3D12Resource*> buffers(desc.BufferCount);
+		auto handle = pDescriptorHeap->GetCPUDescriptorHandleForHeapStart();
+		for (UINT i = 0; i < desc.BufferCount; i++)
+		{
+			pSwapChain->GetBuffer(i, IID_PPV_ARGS(&buffers[i]));
+			handle.ptr += i * pDevice->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+			pDevice->CreateRenderTargetView(buffers[i], nullptr, handle);
+		}
+	}
+}
+
+// DirectXの解放
+void RELEASE_SAFEeaseD3DX()
+{
+	RELEASE_SAFE(pDescriptorHeap);
+	RELEASE_SAFE(pSwapChain);
+	RELEASE_SAFE(pCommandQueue);
+	RELEASE_SAFE(pCommandList);
+	RELEASE_SAFE(pCommandAllocator);
+	RELEASE_SAFE(pDxgiFactory);
+	RELEASE_SAFE(pDevice);
+}
 
 LRESULT WindowProc(HWND, UINT, WPARAM, LPARAM);
 
@@ -13,11 +101,13 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance, 
 	w.hInstance = GetModuleHandle(nullptr);
 	RegisterClassEx(&w);
 
-	RECT wrc = { 0, 0, 640, 480 };
+	RECT wrc = { 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT };
 	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
 
 	HWND hWnd = CreateWindow(className, className, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, wrc.right - wrc.left, wrc.bottom - wrc.top, nullptr, nullptr, w.hInstance, nullptr);
 	ShowWindow(hWnd, SW_SHOW);
+
+	InitD3DX(hWnd);
 
 	MSG msg = {};
 	while (msg.message != WM_QUIT)
@@ -28,6 +118,8 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance, 
 			DispatchMessage(&msg);
 		}
 	}
+
+	RELEASE_SAFEeaseD3DX();
 
 	UnregisterClass(className, w.hInstance);
 	return 0;
