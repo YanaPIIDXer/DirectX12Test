550d3558b002c716383476b2650dc8c01a819f0e
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-29 07:52:18 +0900

    シェーダリソースビューの生成

--- main.cpp
+++ main.cpp
@@ -366,6 +366,15 @@ bool InitD3DX(HWND hWnd)
 		}
 	}
 
+	{
+		D3D12_SHADER_RESOURCE_VIEW_DESC desc = {};
+		desc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+		desc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+		desc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
+		desc.Texture2D.MipLevels = 1;
+
+		pDevice->CreateShaderResourceView(pTextureBuffer, &desc, pTextureDescHeap->GetCPUDescriptorHandleForHeapStart());
+	}
 	return true;
 }
 
