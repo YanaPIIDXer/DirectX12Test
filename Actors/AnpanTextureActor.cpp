20ee8ffc17722493370eddbe4ea26f2c4e091ac2
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-31 03:27:22 +0900

    アンパンを描画するActorをSpawn

--- Actors/AnpanTextureActor.cpp
+++ Actors/AnpanTextureActor.cpp
@@ -5,5 +5,5 @@
 void AnpanTextureActor::Initialize(ID3D12Device* pDevice)
 {
 	TextureComponent* texComponent = new TextureComponent(this, GetScene());
-	texComponent->LoadTexture(pDevice, L"img/Anpan.jpg");
+	texComponent->LoadTexture(pDevice, L"imgs/Anpan.jpg");
 }
--- Scenes/TextureScene.cpp
+++ Scenes/TextureScene.cpp
@@ -1,6 +1,10 @@
 #include "TextureScene.h"
+#include "../Actors/AnpanTextureActor.h"
 
 // ‰Šú‰»
 void TextureScene::Initialize(ID3D12Device* pDevice)
 {
+	AnpanTextureActor* pActor = new AnpanTextureActor(this);
+	pActor->Initialize(pDevice);
+	AddActor(pActor);
 }
