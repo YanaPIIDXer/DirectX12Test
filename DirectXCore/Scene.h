f2085d94b94dfd42eb22d4391373b598042fb5a5
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-31 01:33:01 +0900

    シーン基底クラスをとりあえず定義

---
+++ DirectXCore/Scene.h
@@ -0,0 +1,21 @@
+#ifndef SCENE_H
+#define SCENE_H
+
+// シーン基底クラス
+class Scene {
+
+public:
+
+	// コンストラクタ
+	Scene();
+
+	// デストラクタ
+	virtual ~Scene() = 0;
+
+protected:
+
+private:
+
+};
+
+#endif	// #ifndef SCENE_H
