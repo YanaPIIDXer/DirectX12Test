e8b5f0986c65214cd91d0b6dcf51b0eb1882d2cb
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-31 03:18:21 +0900

    とりあえず初期化関数

--- Scenes/TextureScene.cpp
+++ Scenes/TextureScene.cpp
@@ -1,1 +1,6 @@
 #include "TextureScene.h"
+
+// ‰Šú‰»
+void TextureScene::Initialize(ID3D12Device* pDevice)
+{
+}
--- Scenes/TextureScene.h
+++ Scenes/TextureScene.h
@@ -5,6 +5,10 @@
 // テクスチャ描画シーン
 class TextureScene : public Scene
 {
+public:
+
+	// 初期化
+	virtual void Initialize(ID3D12Device* pDevice) override;
 };
 
 #endif		// #ifndef TEXTURE_SCENE
